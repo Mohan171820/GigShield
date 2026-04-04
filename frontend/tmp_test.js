dd2b8f4bbac192bebe03a3951d1fef094b3fda28
S.Shri Raksha <shriraksha2107@gmail.com>
2026-04-04 18:04:45 +0530

    Delete frontend directory

--- frontend/tmp_test.js
+++
@@ -1,8 +0,0 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-async function test() {
-  const res = await fetch('http://26.37.211.123:8080/api/v1/payouts');
-  const data = await res.json();
-  console.log('PAYOUT KEYS:', Object.keys(data[0] || {}));
-  console.log('SAMPLE PAYOUT:', data[0]);
-}
-test();
